4cf21a7526838ed063841ba926d24fb9f270ca24
junamon <fren_efull@hotmail.com>
2016-04-26 11:27:54 +0700

    ลิงค์ Sign up ไปหน้า สมัครใช้งาน

--- app/src/main/java/ice_pbru/junamonduangsang/ice_database/MainActivity.java
+++ app/src/main/java/ice_pbru/junamonduangsang/ice_database/MainActivity.java
@@ -1,13 +1,32 @@
 package ice_pbru.junamonduangsang.ice_database;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //เริ่ม bind Widget
+        bindWidget();
+
+
+}//Main Method
+
+    public void clickRegis(View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
     }
-}
+
+    private void bindWidget() {
+
+
+
+    }// จบ bind Widget
+
+
+}//Main class
