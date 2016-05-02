92af755e3371f6b6983233d49374aa592dd2e8ac
junamon <fren_efull@hotmail.com>
2016-05-02 15:36:46 +0700

    MyJSON

--- app/src/main/java/ice_pbru/junamonduangsang/ice_database/MainActivity.java
+++ app/src/main/java/ice_pbru/junamonduangsang/ice_database/MainActivity.java
@@ -1,10 +1,21 @@
 package ice_pbru.junamonduangsang.ice_database;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     //Explicit
@@ -20,11 +31,78 @@ public class MainActivity extends AppCompatActivity {
         bindWidget();
 
         mySQLite = new MySQLite(this);
+        //Synchronize mySQL to SQLite
+        synAndDelete();
 
 
 
 
 }//Main Method
+
+    private void synAndDelete() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name, MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MySQLite.User_table, null, null);
+
+        MySynJSON mySynJSON = new MySynJSON();
+        mySynJSON.execute();
+
+
+    }//synAndDelete
+
+    public class MySynJSON extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                String strURL = "http://ice.pbru.ac.th/ICE56/Junamon002/php_get_user.php";
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strURL).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e ) {
+                Log.d("Juamon-->", "doInback-->" + e.toString());
+                return null;
+            }
+
+        }// do in background
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("Junamon -->", "strJSON -->" + s);
+
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String strName = jsonObject.getString(MySQLite.colum_Name);
+                    String strSurname =
+                            jsonObject.getString(MySQLite.colum_Surname);
+                    String strUser = jsonObject.getString(MySQLite.colum_User);
+                    String strPassword = jsonObject.getString(MySQLite.colum_Password);
+                    String strEmail = jsonObject.getString(MySQLite.colum_Email);
+
+                    mySQLite.addNewUser(strName, strSurname, strUser, strPassword, strEmail);
+
+                    Log.d("Junamon-->", "Name =" + strName.toString());
+                }
+                Toast.makeText(MainActivity.this, "Synchronize mySQL to SQLite Finish",
+                        Toast.LENGTH_LONG).show();
+
+
+            } catch (Exception e) {
+                Log.d("Junamon-->", "onPost-->" + e.toString());
+            }
+
+
+        }//on post
+
+    }// mySyJSON
+
+
+
 
     public void clickSinIn(View view) {
         userString = userEditText.getText().toString().trim();
